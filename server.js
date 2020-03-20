a3af82344a2b006d655ccc5986348f545869f831
Andrew Bjork <bjorkandrew1@gmail.com>
2020-03-19 22:45:27 -0500

    test

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
-
-
 var orm = require("./config/orm.js");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "quotes_db"
+});
