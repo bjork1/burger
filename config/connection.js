9f44b1618ecc2ebdd105cf3197206de04921838f
Andrew Bjork <bjorkandrew1@gmail.com>
2020-03-30 20:58:47 -0500

    heroku db info

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,5 @@
 var mysql = require("mysql");
-
+/*
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -7,13 +7,14 @@ var connection = mysql.createConnection({
   password: "",
   database: "burgers_db"
 });
-
-module.exports = {
-  HOST: "us-cdbr-iron-east-01.cleardb.net",
-  USER: "bac8b43aee7a0b",
-  PASSWORD: "4159c8d6",
-  DB: "heroku_0e23a815733059d"
-};
+*/
+var connection = mysql.createConnection({
+  host: "us-cdbr-iron-east-01.cleardb.net",
+  port: 3306,
+  user: "bac8b43aee7a0b",
+  password: "4159c8d6",
+  database: "heroku_0e23a815733059d"
+});
 
 connection.connect(function(err) {
   if (err) {
